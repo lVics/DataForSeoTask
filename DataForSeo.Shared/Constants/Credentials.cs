48afb15fc0e9fffb70f7d5b4ea7f215770994520
NikitaBelyaev <thewalkingundeads@gmail.com>
2021-03-25 11:41:51 +0200

    remove creds

--- DataForSeo.Shared/Constants/Credentials.cs
+++ DataForSeo.Shared/Constants/Credentials.cs
@@ -2,7 +2,7 @@
 {
   public class Credentials
   {
-    public const string UserName = "challenger28@rankactive.info"; 
-    public const string Password = "W8DQXc6V5"; 
+    public const string UserName = "UserName"; 
+    public const string Password = "Password"; 
   }
 }
